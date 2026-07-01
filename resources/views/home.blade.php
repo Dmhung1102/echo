731bfffed83f8212016a0e66b36a68a8ede29d0d
dmhung <dmhung1122@gmail.com>
2026-07-01 16:50:31 +0700

    hung dm commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -233,6 +233,7 @@
                                 <ul>
                                     <li>
                                         <div class="content p-0 order-2">
+                                            <span class="banner-side-tag text-uppercase fw-bold">GAME GUIDES</span>
                                             <h3 class="title-latest">
                                                 <a href="" class="title title-hover-white">
                                                     Jump into Wayfinder 23
@@ -253,6 +254,7 @@
                                     </li>
                                     <li>
                                         <div class="content p-0 order-2">
+                                            <span class="banner-side-tag text-uppercase fw-bold">TECHNOLOGY</span>
                                             <h3 class="title-latest">
                                                 <a href="" class="title title-hover-white">
                                                     Jump into Wayfinder 23
@@ -514,7 +516,7 @@
                 <div class="col-lg-4">
                     <div class="row">
                         <div class="col-lg-12">
-                            <div class="title-area right mb-5 text-theme-light">
+                            <div class="title-area right mb-4 text-theme-light">
                                 <h3>
                                     most popular
                                 </h3>
@@ -525,14 +527,14 @@
                                 <ul>
                                     <li>
                                         <div class="content p-0 order-2">
+                                            <span class="banner-side-tag text-uppercase fw-bold">GAME GUIDES</span>
                                             <h3 class="">
                                                 <a href="" class="title text-theme-light">
-                                                    Jump into Wayfinder 23
+                                                    Can Players Unlock Na'el In Xenoblade
                                                 </a>
                                             </h3>
                                             <p class="">
-                                                <a href="#" class="desc"><i class="fa-solid fa-calendar me-2"></i>01 Jan
-                                                    2023</a>
+                                                <a href="#" class="desc"><i class="fa-solid fa-calendar me-2"></i>01 Jan 2023</a>
                                             </p>
                                         </div>
                                         <div class="">
@@ -545,14 +547,14 @@
                                     </li>
                                     <li>
                                         <div class="content p-0 order-2">
-                                            <h3 class="title-latest">
+                                            <span class="banner-side-tag text-uppercase fw-bold">GAME GUIDES</span>
+                                            <h3 class="">
                                                 <a href="" class="title text-theme-light">
-                                                    Jump into Wayfinder 23
+                                                    Can Players Unlock Na'el In Xenoblade
                                                 </a>
                                             </h3>
                                             <p class="">
-                                                <a href="#" class="desc"><i class="fa-solid fa-calendar me-2"></i>01 Jan
-                                                    2023</a>
+                                                <a href="#" class="desc"><i class="fa-solid fa-calendar me-2"></i>01 Jan 2023</a>
                                             </p>
                                         </div>
                                         <div class="">
@@ -565,14 +567,14 @@
                                     </li>
                                     <li>
                                         <div class="content p-0 order-2">
-                                            <h3 class="title-latest">
+                                            <span class="banner-side-tag text-uppercase fw-bold">GAME GUIDES</span>
+                                            <h3 class="">
                                                 <a href="" class="title text-theme-light">
-                                                    Jump into Wayfinder 23
+                                                    Can Players Unlock Na'el In Xenoblade
                                                 </a>
                                             </h3>
                                             <p class="">
-                                                <a href="#" class="desc"><i class="fa-solid fa-calendar me-2"></i>01 Jan
-                                                    2023</a>
+                                                <a href="#" class="desc"><i class="fa-solid fa-calendar me-2"></i>01 Jan 2023</a>
                                             </p>
                                         </div>
                                         <div class="">
@@ -586,9 +588,232 @@
                                 </ul>
                             </div>
                         </div>
+                        <div class="col-lg-12">
+                            <div class="title-area right mb-4 text-theme-light">
+                                <h3 class="text-capitalize">
+                                    gallery
+                                </h3>
+                            </div>
+                            <div class="gallery-widget">
+                                <div class="row g-3">
+                                    <div class="col-6">
+                                        <a href="#" class="gallery-item">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_2ooBBf1ZmIk36uDZPKpI8nKeSbs5jr0uAA&s" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="col-6">
+                                        <a href="#" class="gallery-item">
+                                            <img src="https://funix.edu.vn/wp-content/uploads/2023/07/Game-HTML5.jpg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="col-6">
+                                        <a href="#" class="gallery-item">
+                                            <img src="https://funix.edu.vn/wp-content/uploads/2023/07/Game-HTML5.jpg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="col-6">
+                                        <a href="#" class="gallery-item">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_2ooBBf1ZmIk36uDZPKpI8nKeSbs5jr0uAA&s" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="tip-section">
+        <div class="container">
+            <div class="row">
+                <!-- Left Column: Tips & Guides -->
+                <div class="col-lg-8">
+                    <div class="title-area mb-4">
+                        <h2 class="text-capitalize fw-semibold text-theme-light header m-0">
+                            <span class="tip-title-icon"><i class="fa-solid fa-lightbulb"></i></span>
+                            Tip's & Guides
+                        </h2>
+                    </div>
+                    <div class="tip-list mt-4">
+                        <!-- Tip Item 1 -->
+                        <div class="tip-item d-flex gap-4 mb-4">
+                            <div class="tip-img-wrapper flex-shrink-0">
+                                <a href="" class="content-img h-100">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_2ooBBf1ZmIk36uDZPKpI8nKeSbs5jr0uAA&s" alt="" class="tip-img">
+                                </a>
+                            </div>
+                            <div class="tip-content d-flex flex-column justify-content-center">
+                                <div>
+                                    <span class="badge-tag text-uppercase">game guides</span>
+                                </div>
+                                <h3 class="tip-title truncate-2-line">
+                                    <a href="" class="title-hover text-theme-light text-capitalize">
+                                        <span class="title-hover">10 Critical Things To Know Before Playing...</span>
+                                    </a>
+                                </h3>
+                                <div class="latest-news-time-views p-0 border-0 mt-2">
+                                    <a href="#" class="pe-none"><i class="fa-solid fa-calendar me-2"></i>01 Jan 2023</a>
+                                    <a href="#" class="pe-none"><i class="fa-solid fa-comment me-2"></i> 05 Comments</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Tip Item 2 -->
+                        <div class="tip-item d-flex gap-4 mb-4">
+                            <div class="tip-img-wrapper flex-shrink-0">
+                                <a href="" class="content-img h-100">
+                                    <img src="https://funix.edu.vn/wp-content/uploads/2023/07/Game-HTML5.jpg" alt="" class="tip-img">
+                                </a>
+                            </div>
+                            <div class="tip-content d-flex flex-column justify-content-center">
+                                <div>
+                                    <span class="badge-tag text-uppercase">game guides</span>
+                                </div>
+                                <h3 class="tip-title truncate-2-line">
+                                    <a href="" class="title-hover text-theme-light text-capitalize">
+                                        <span class="title-hover">9 Things We Wish We Knew Before Playing...</span>
+                                    </a>
+                                </h3>
+                                <div class="latest-news-time-views p-0 border-0 mt-2">
+                                    <a href="#" class="pe-none"><i class="fa-solid fa-calendar me-2"></i>01 Jan 2023</a>
+                                    <a href="#" class="pe-none"><i class="fa-solid fa-comment me-2"></i> 05 Comments</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Tip Item 3 -->
+                        <div class="tip-item d-flex gap-4 mb-4">
+                            <div class="tip-img-wrapper flex-shrink-0">
+                                <a href="" class="content-img h-100">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_2ooBBf1ZmIk36uDZPKpI8nKeSbs5jr0uAA&s" alt="" class="tip-img">
+                                </a>
+                            </div>
+                            <div class="tip-content d-flex flex-column justify-content-center">
+                                <div>
+                                    <span class="badge-tag text-uppercase">game guides</span>
+                                </div>
+                                <h3 class="tip-title truncate-2-line">
+                                    <a href="" class="title-hover text-theme-light text-capitalize">
+                                        <span class="title-hover">14 Tight Samurai Games You Should Play</span>
+                                    </a>
+                                </h3>
+                                <div class="latest-news-time-views p-0 border-0 mt-2">
+                                    <a href="#" class="pe-none"><i class="fa-solid fa-calendar me-2"></i>01 Jan 2023</a>
+                                    <a href="#" class="pe-none"><i class="fa-solid fa-comment me-2"></i> 05 Comments</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-center mt-5">
+                        <a href="#" class="rts-btn see-all-btn">
+                            Show More <i class="bi bi-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <!-- Right Column: Top Games -->
+                <div class="col-lg-4">
+                    <div class="title-area right mb-4 text-theme-light">
+                        <h3 class="text-capitalize">
+                            top games
+                        </h3>
+                    </div>
+                    <div class="top-games-widget mt-4">
+                        <!-- Large Card -->
+                        <div class="top-game-large position-relative rounded-4 overflow-hidden mb-4">
+                            <img src="https://funix.edu.vn/wp-content/uploads/2023/07/Game-HTML5.jpg" alt="" class="top-game-large-bg w-100 h-100">
+                            <div class="top-game-large-overlay"></div>
+                            <div class="top-game-large-content d-flex flex-column justify-content-end p-4 position-absolute start-0 top-0 w-100 h-100">
+                                <div>
+                                    <span class="badge-tag text-uppercase">pc game</span>
+                                </div>
+                                <h3 class="text-white fw-bold mb-2">
+                                    <a href="" class="title-hover-white text-white text-capitalize">
+                                        Games Moving Away From Gacha
+                                    </a>
+                                </h3>
+                                <div class="shares text-white-50 fs-7">
+                                    <i class="fa-solid fa-share-nodes me-2"></i>100+K Shares
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Item 02 -->
+                        <div class="top-game-item d-flex align-items-center p-3 mb-3">
+                            <span class="top-game-num">02</span>
+                            <div class="top-game-info flex-grow-1">
+                                <h4 class="top-game-title truncate-1-line mb-1">
+                                    <a href="" class="title-hover text-theme-light">
+                                        Forspoken | A Square Enix Open-World RPG...
+                                    </a>
+                                </h4>
+                                <div class="shares text-muted fs-7">
+                                    <i class="fa-solid fa-share-nodes me-2"></i>100+K Shares
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Item 03 -->
+                        <div class="top-game-item d-flex align-items-center p-3 mb-3">
+                            <span class="top-game-num">03</span>
+                            <div class="top-game-info flex-grow-1">
+                                <h4 class="top-game-title truncate-1-line mb-1">
+                                    <a href="" class="title-hover text-theme-light">
+                                        Kerbal Space Program 2 | Your Favorite...
+                                    </a>
+                                </h4>
+                                <div class="shares text-muted fs-7">
+                                    <i class="fa-solid fa-share-nodes me-2"></i>100+K Shares
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="download-section py-5">
+        <div class="container">
+            <div class="download-banner position-relative overflow-hidden py-5 px-4 px-md-5">
+                <div class="row align-items-center position-relative z-index-2">
+                    <!-- Left Content -->
+                    <div class="col-lg-6 col-md-7 text-white mb-4 mb-lg-0">
+                        <h2 class="text-white fw-bold mb-3 display-6 text-capitalize">
+                            Download Our Apps For Stay Update
+                        </h2>
+                        <p class="text-white-50 mb-4 pe-md-4">
+                            Easy to update latest news, daily podcast and everything in your hand
+                        </p>
+                        <div class="d-flex flex-wrap gap-3">
+                            <a href="#" class="download-btn">
+                                <i class="fa-brands fa-apple"></i>
+                                <span>
+                                    <small>Download on the</small>
+                                    <strong>Apple Store</strong>
+                                </span>
+                            </a>
+                            <a href="#" class="download-btn">
+                                <i class="fa-brands fa-google-play"></i>
+                                <span>
+                                    <small>Get it on</small>
+                                    <strong>Play Store</strong>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <!-- Right Mockup -->
+                    <div class="col-lg-6 col-md-5 text-center text-md-end">
+                        <div class="mockup-wrapper">
+                            <img src="https://html.themewant.com/echo/assets/images/home-1/cta/phone.png" alt="" class="img-fluid mockup-img">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-section">
+
     </div>
 @endsection
---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,65 @@
+<footer class="footer-area py-5 text-white">
+    <div class="container">
+        <!-- Top Section: Newsletter -->
+        <div class="row align-items-center pb-4 mb-4 border-bottom border-secondary border-opacity-25">
+            <div class="col-lg-6 mb-4 mb-lg-0">
+                <h3 class="text-white fw-bold mb-0 lh-base text-capitalize footer-newsletter-title">
+                    Get the gaming world News & Stories into your inbox
+                </h3>
+            </div>
+            <div class="col-lg-6">
+                <form action="#" class="footer-newsletter-form d-flex align-items-center">
+                    <div class="position-relative flex-grow-1 d-flex align-items-center">
+                        <i class="fa-regular fa-envelope input-icon"></i>
+                        <input type="email" placeholder="Enter your email" required class="form-control">
+                    </div>
+                    <button type="submit" class="subscribe-btn text-capitalize">Subscribe</button>
+                </form>
+                <p class="newsletter-policy mt-2 mb-0">
+                    By subscribing, you accepted the our <a href="#" class="text-decoration-underline text-white-50">Policy</a>
+                </p>
+            </div>
+        </div>
+
+        <!-- Middle Section: Brand & Socials -->
+        <div class="text-center py-4">
+            <div class="footer-logo mb-4">
+                <img src="https://html.themewant.com/echo/assets/images/home-1/site-logo/site-logo4.svg" alt="Echo Logo" class="footer-logo-img">
+            </div>
+            <div class="footer-social-links mb-4 d-flex justify-content-center gap-3">
+                <a href="#" class="social-box"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#" class="social-box"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#" class="social-box"><i class="fa-brands fa-linkedin-in"></i></a>
+                <a href="#" class="social-box"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#" class="social-box"><i class="fa-brands fa-pinterest-p"></i></a>
+                <a href="#" class="social-box"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+        </div>
+
+        <!-- Navigation Links -->
+        <div class="footer-nav mb-4 text-center">
+            <ul class="d-flex flex-wrap justify-content-center align-items-center gap-3 list-unstyled">
+                <li><a href="#" class="footer-link">Home</a></li>
+                <li class="separator">/</li>
+                <li><a href="#" class="footer-link">Game</a></li>
+                <li class="separator">/</li>
+                <li><a href="#" class="footer-link">VR Game</a></li>
+                <li class="separator">/</li>
+                <li><a href="#" class="footer-link">AR Game</a></li>
+                <li class="separator">/</li>
+                <li><a href="#" class="footer-link">Mobile Game</a></li>
+                <li class="separator">/</li>
+                <li><a href="#" class="footer-link">E-Sports</a></li>
+                <li class="separator">/</li>
+                <li><a href="#" class="footer-link">Contact Us</a></li>
+            </ul>
+        </div>
+
+        <!-- Copyright Row -->
+        <div class="text-center pt-4 border-top border-secondary border-opacity-25 mt-4">
+            <p class="copyright-text mb-0">
+                &copy; Copyright 2026 by Echo
+            </p>
+        </div>
+    </div>
+</footer>
